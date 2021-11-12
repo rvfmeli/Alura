a4e5ba82447310b2708c25659a9f2c70f6da7c45
rvfmeli <rafael.vicente@mercadolivre.com>
2021-11-12 03:28:41 -0300

    criado classes conta, criarConta e metodo

---
+++ src/main/java/banco/Conta.java
@@ -0,0 +1,48 @@
+package banco;
+
+public class Conta {
+
+
+    public int saldo;
+    public int agencia;
+    public boolean numero;
+
+    public static void main(String[] args) {
+
+        int saldo;
+
+        int agencia;
+        int numero;
+        String titular;
+
+        public void deposita ( double valor){
+        }
+        this.saldo += valor;
+    }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/banco/TestaMetodo.java
+++ src/main/java/banco/TestaMetodo.java
@@ -1,11 +1,14 @@
-package iniciante;
-
+package banco;
 
 public class TestaMetodo {
     public static void main(String[] args) {
+
         Conta contaDoPaulo = new Conta();
         contaDoPaulo.saldo = 100;
         contaDoPaulo.deposita(50);
         System.out.println(contaDoPaulo.saldo);
+
+
+
     }
 }
